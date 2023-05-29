32aa3b5ab4e77d7e993d4d3f359c221e0117feb2
Alex Sánchez <info@alexsanchez.dev>
2023-05-29 12:20:46 +0200

    fix: Update border

--- lib/date_widget.dart
+++ lib/date_widget.dart
@@ -31,10 +31,16 @@ class DateWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      borderRadius: BorderRadius.all(
+        Radius.circular(6),
+      ),
       child: Container(
         width: width,
         margin: EdgeInsets.all(2.0),
         decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(6),
+          ),
           color: selectionColor,
         ),
         child: Padding(
